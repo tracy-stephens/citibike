337e7ab57f74bb2d9e67056db1d0a9cc64a2d17e
tracy-stephens <tracystephens972@gmail.com>
2022-10-02 23:10:52 -0400

    real time snapshots

--- citibike.py
+++ citibike.py
@@ -9,7 +9,8 @@ from zipfile import ZipFile, BadZipfile
 import io
 
 
-DATA_DIR = os.path.join(os.getcwd(), "trip_data", "raw")
+TRIP_DATA_DIR = os.path.join(os.getcwd(), "trip_data", "raw")
+SNAPSHOTS_DIR = os.path.join(os.getcwd(), "snapshots")
 GBFS_URL = "http://gbfs.citibikenyc.com/gbfs/gbfs.json"
 TRIPDATA_URL = "https://s3.amazonaws.com/tripdata"
 
@@ -24,6 +25,11 @@ def read_json(url):
     res = urllib.request.urlopen(url).read()
     return json.loads(res)
 
+
+def write_json(dict_, file_name):
+    with open(file_name, "w") as outfile:
+        json.dump(dict_, outfile)
+    
 
 def read_zip_file(url):
     response = requests.get(url, stream=True, verify=False)
@@ -101,7 +107,7 @@ class Station:
 
 
 class TripData:
-    def __init__(self, month: str, data_dir=DATA_DIR, trip_data_url=TRIPDATA_URL):
+    def __init__(self, month: str, data_dir=TRIP_DATA_DIR, trip_data_url=TRIPDATA_URL):
         self.file_name = f"{str(month)}-citibike-tripdata.csv"
         self.file_path = os.path.join(data_dir, self.file_name)
         self.start = datetime(int(month[:4]), int(month[4:]), 1)
@@ -177,13 +183,22 @@ class TripData:
     
 
 class RealTimeData:
-    def __init__(self, url=GBFS_URL):
+    def __init__(
+        self, 
+        url=GBFS_URL, 
+        data_dir=SNAPSHOTS_DIR,
+        source='local'
+    ):
         self.url = url
+        self.data_dir = data_dir
+        self.source = source 
+        
         self.language = 'en'
         
         self.last_update_time = None
         self.ttl = None
-        
+        self.saved = False
+
         self._url_map = None
         self._feeds = None
         
@@ -208,10 +223,47 @@ class RealTimeData:
             self.update()
         return self._feeds
     
+    def save(self, dir=None):
+        if dir is None:
+            dir_ = self.data_dir
+        else:
+            dir_ = dir
+
+        snapshot_time = datetime.now().timestamp()
+        save_dir = os.path.join(dir_, str(snapshot_time))
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        for name, url in self.feeds.items():
+            data = read_json(url)
+            write_json(data, os.path.join(save_dir, name+".json"))
+        self.saved = True
+
+
+class SystemInformation(RealTimeData):
+    def __init__(self, url=GBFS_URL):
+
+        self._data = None
+        super().__init__(url=url)
     
+    def update(self):
+        super().update()
+        url = self._feeds['system_information']
+        res = read_json(url)
+        self._data = res
+        self.last_update_time = res['last_updated']
+        self.ttl = res['ttl']
+    
+    @property
+    def data(self):
+        if self._data is None:
+            self.update()
+        return self._data
+    
+
 class StationInformation(RealTimeData):
     def __init__(self, url=GBFS_URL):
         
+        self._data = None
         self._stations = None
         super().__init__(url=url)
     
@@ -219,9 +271,16 @@ class StationInformation(RealTimeData):
         super().update()
         url = self._feeds['station_information']
         res = read_json(url)
+        self._data = res
         self._stations = res['data']['stations']
         self.last_update_time = res['last_updated']
         self.ttl = res['ttl']
+    
+    @property
+    def data(self):
+        if self._data is None:
+            self.update()
+        return self._data
     
     @property
     def stations(self):
@@ -233,6 +292,7 @@ class StationInformation(RealTimeData):
 class StationStatus(RealTimeData):
     def __init__(self, url=GBFS_URL):
         
+        self._data = None
         self._stations = None
         super().__init__(url=url)
     
@@ -240,16 +300,24 @@ class StationStatus(RealTimeData):
         super().update()
         url = self._feeds['station_status']
         res = read_json(url)
+        self._data = res
         self._stations = res['data']['stations']
         self.last_update_time = res['last_updated']
         self.ttl = res['ttl']
+    
+    @property
+    def data(self):
+        if self._data is None:
+            self.update()
+        return self._data
     
     @property
     def stations(self):
         if self._stations is None:
             self.update()
         return self._stations
-        
+
+
         
     
     
